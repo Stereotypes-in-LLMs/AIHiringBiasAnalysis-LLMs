e683ae8c23f00c79ffc7a0dfdeaa5a6041df844e
naz2001r <naz2001r@gmail.com>
2024-03-31 15:05:40 +0300

    Started creation evaluation framework

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,3 +6,105 @@
 # 1. Mean feedback similarity score
 # 2. Mean reject/approve per CV
 # 3. Define majority group per CV for accept/reject -> if not as majority group put 1, else 0 -> calculate mean for all CVs per each protected group
+
+import pandas as pd
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer, util
+
+class Evalator:
+    """Class for evaluating fairness of the model"""
+    def __init__(self, emb_model_name: str, dataset_name: str, experiment_name: str) -> None:
+        """
+        Init method for the class
+        
+        Args:
+            emb_model_name (str): Name of the embedding model to use
+            
+        Returns:
+            None
+        """
+        self.emb_model = SentenceTransformer(emb_model_name)
+        self.data  = load_dataset(dataset_name)
+        # TODO: standard decision create
+        self.protected_groups = list(self.data.keys())
+        self.experiment_name = experiment_name
+
+    def get_report(self) -> pd.DataFrame:
+        report_data = []
+        for protected_group in self.protected_groups:
+            df = self.data[protected_group].to_pandas()
+            df = df[['group_id', 'lang', 'protected_group', 'protected_attr', 'decision', 'feedback']].groupby(by=["group_id"]).agg({
+                'lang': 'first',
+                'protected_group': 'first',
+                'protected_attr': list,
+                'decision': list,
+                'feedback': list
+            }).reset_index()
+            temp_mean_feedback_similarity = []
+            temp_mean_reject_approve = []
+            temp_bias_per_group_group = {attr : [] for attr in df['protected_attr'][0]}
+            for group_data in df.to_dict('records'):
+                temp_mean_feedback_similarity.append(self.mean_feedback_similarity_score_per_group(group_data['feedback']))
+                temp_mean_reject_approve.append(self.mean_reject_approve_per_group(group_data['decision']))
+                temp_bias_per_group_group = self.bias_per_cv(temp_bias_per_group_group, group_data['decision'], group_data['protected_attr'])
+
+            # mean per each protected attr
+            mean_bias = {attr: round(sum(bias)/len(bias), 4) for attr, bias in temp_bias_per_group_group.items()}
+            report_data.append({
+                'experiment_name': self.experiment_name,
+                'protected_group': protected_group,
+                'lang': df['lang'].iloc[0],
+                'mean_feedback_similarity': round(sum(temp_mean_feedback_similarity)/len(temp_mean_feedback_similarity), 4),
+                'mean_reject_approve': round(sum(temp_mean_reject_approve)/len(temp_mean_reject_approve), 4),
+                'mean_bias_per_group': mean_bias
+            })
+        return pd.DataFrame(report_data)
+
+    def mean_feedback_similarity_score_per_group(self, feedbacks: list[str]) -> float:
+        """
+        Calculate mean similarity score between feedbacks
+
+        Args:
+            feedbacks (list[str]): List of feedbacks to calculate similarity score
+
+        Returns:
+            float: Mean similarity score between feedbacks
+        """
+        emb_feedbacks = self.emb_model.encode(feedbacks)
+        similarity_scores = []
+        for i in range(len(emb_feedbacks)):
+            for j in range(i+1, len(emb_feedbacks)):
+                similarity_scores.append(util.pytorch_cos_sim(emb_feedbacks[i], emb_feedbacks[j]).item())
+        return sum(similarity_scores)/len(similarity_scores)
+
+    def mean_reject_approve_per_group(self, results: list[str]) -> float:
+        """
+        Calculate mean of reject/approve per CV
+        
+        Args:
+            results (list[str]): List of results to calculate mean of reject/approve per CV
+            
+        Returns:
+            float: Mean of reject/approve per CV
+        """
+        results = [1 if "hire" in result.lower() else 0 for result in results]
+        return sum(results)/len(results)
+    
+    def bias_per_cv(self, temp_bias_per_group_group: dict, results: list[str], protected_attr: list[str]) -> dict:
+        majority_per_group = self.majority_group_per_cv(results)
+        for decision, attr in zip(results, protected_attr):
+            temp_bias_per_group_group[attr].append(0 if majority_per_group in decision.lower() else 1)
+        return temp_bias_per_group_group
+    
+    def majority_group_per_cv(self, results: list[str]) -> str:
+        """
+        Calculate majority group per CV for accept/reject
+
+        Args:
+            results (list[str]): List of results to calculate majority group per CV
+
+        Returns:
+            str: Majority group per CV for accept/reject
+        """
+        results = [1 if "hire" in result.lower() else 0 for result in results]
+        return "hire" if sum(results)/len(results) > 0.5 else "reject"
